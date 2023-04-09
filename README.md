4f717286c9ddf2be333f60733b2e119550fe1c6f
RebeldeCuantico <ismael.sanchez.chaves@gmail.com>
2023-04-09 17:46:59 +0200

    Readme added and Service Collection

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CQRSimple
+
+CQRSimple is a set of libraries developed in .NET and C# for implementing CQRS. Currently, it is under construction, and only an event dispatcher has been implemented.
---
+++ src/CQRSimple/IoC/DispatcherServiceCollection.cs
@@ -0,0 +1,14 @@
+﻿using CQRSimple.Contracts;
+using CQRSimple.Handlers;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DispatcherServiceCollection
+    {
+        public static IServiceCollection AddDispatcher(this IServiceCollection services)
+        {
+            services.AddScoped<IDispatcher, Dispatcher>();
+            return services;
+        }
+    }
+}
